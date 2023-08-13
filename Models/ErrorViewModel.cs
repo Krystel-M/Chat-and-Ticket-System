864df4c3466c7a8020ee00a83d3c1ab0bf66fffd
Krystel Morales <krystelmorales72@outlook.com>
2023-08-13 15:28:10 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Chat_and__Ticket_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
